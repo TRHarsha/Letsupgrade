f00c695c7fbf043573dac1eca90ada95767d8199
Htr1172 <63777708+Htr1172@users.noreply.github.com>
2022-11-04 22:01:41 +0530

    Assignment 2

---
+++ LU_P_A2.py
@@ -0,0 +1,10 @@
+def reverse(s):
+	str = ""
+	for i in s:
+		str = i + str
+	return str
+s = "Letsupgrade"
+print("The original string is : ", end="")
+print(s)
+print("The reversed string(using loops) is : ", end="")
+print(reverse(s))
